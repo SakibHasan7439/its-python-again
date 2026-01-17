7dd50de8586d0fa891ded5f8adc725df7501d903
Sakib hasan <sakibhasan16h@gmail.com>
2026-01-17 18:33:24 +0600

    more python

--- first-day.py
+++ first-day.py
@@ -1,2 +1,24 @@
 color = input("whats the color of rose?");
 print(color);
+
+# typecasting
+numberOfRose = int(input("Enter the number of rose: "));
+print("There are ", numberOfRose, "Roses in the bucket and the roses were", color);
+
+priceOfRoses = float(input("Enter the price of the roses: "));
+print(priceOfRoses)
+
+# datatypes
+a = "Im string";
+b = 323;
+c = 4.0
+d = ('apple', 'banana', 'mango');
+e = {'rose': 20, 'belly': 10, 'sunflower': 30};
+f = ['ami', 'bala', 'achi'];
+
+print(type(a));
+print(type(b));
+print(type(c));
+print(type(d));
+print(type(e));
+print(type(f));
